416fa32aba26ebe2702c885142ae1e8ea0205a30
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2018-06-24 14:29:03 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,3 @@
+// db.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://root:password1@ds257470.mlab.com:57470/rest-api-tutorial')
---
+++ user/UserController.js
@@ -0,0 +1,60 @@
+// UserController.js
+
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended:  true }));
+router.use(bodyParser.json());
+
+var User = require('./User');
+
+// Creates a new user
+router.post('/', function(req, res) {
+    console.log(req);
+    console.log("name: " + req.body.name);
+    User.create({
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    },
+    function(err, user) {
+        if(err) return res.status(500).send("There was a problem adding the information to the database.");
+        res.status(200).send(user);
+        console.log(user);
+    });
+});
+
+// Returns all the users in the database
+router.get('/', function(req, res) {
+    User.find({}, function(err, users) {
+        if(err) return res.status(500).send("There was a problem finding the users.");
+        res.status(200).send(users);
+    });
+});
+
+// Gets a single user from the database
+router.get('/:id', function(req, res) {
+    User.findById(req.params.id, function (err, user) {
+        if(err) return res.status(500).send("There was a problem finding the user.");
+        if(!user) return res.status(404).send("No user found.");
+        res.status(200).send(user);
+    });
+});
+
+// Deletes a user from the database
+router.delete('/:id', function(req, res) {
+    User.findByIdAndRemove(req.params.id, function(err, user) {
+        if(err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User " + user.name + " was deleted");
+    });
+});
+
+// Updates a single user in the database
+router.put('/:id', function(req, res) {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, user) {
+        if(err) return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+    });
+});
+
+module.exports = router;
